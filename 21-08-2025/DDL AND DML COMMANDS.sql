1e80528aeb9b38a923f0de95e003a30c8a5cce47
ariharsudhan <ariharasudhan123@gmail.com>
2025-08-23 13:48:07 +0530

    Add files via upload

---
+++ 21-08-2025/DDL AND DML COMMANDS.sql
@@ -0,0 +1,24 @@
+show databases;
+use michael;
+create table ECE (
+sid int,
+sname varchar(30),
+smarks int,
+slocation varchar(30)
+);
+show tables from michael;
+desc ECE;
+select * from ECE;
+alter table ECE;
+INSERT INTO ECE (sid, sname, smarks, slocation)
+VALUES 
+(2, 'John', 88, chennai),
+(3, 'Sarah', 92, banglore),
+(4, 'David', 76, mumbai);
+SELECT * FROM ECE;
+
+alter table ECE rename column
+	smark1 to smarks;
+
+update ECE SET smarks=12
+WHERE sname='john';
