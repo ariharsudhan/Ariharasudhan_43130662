6bb5ccc78d92ba68d683d6ef09072ab2c633af03
ariharsudhan <ariharasudhan123@gmail.com>
2025-08-25 12:02:16 +0530

    Update foreign key.sql

--- 23-08-2025/foreign key.sql
+++ 23-08-2025/foreign key.sql
@@ -17,8 +17,7 @@ CREATE TABLE Product (
     pDesc VARCHAR(255),
     cID INT,
     FOREIGN KEY (cID) REFERENCES Category(cID)
-        ON DELETE CASCADE 
-        ON UPDATE CASCADE
+        
 );
 
 -- Step 4: Insert categories
